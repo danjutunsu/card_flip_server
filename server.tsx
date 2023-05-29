6f1f29b36a8539983d3d37401f6b57926aad291a
danjutunsu <33231608+danjutunsu@users.noreply.github.com>
2023-05-29 09:54:59 -0700

    changed listening port

--- server.tsx
+++ server.tsx
@@ -955,5 +955,6 @@ async function getUsernameByID(id) {
   }
 }
 
-app.listen(3001, () => console.log('Server started on port 3001'));
+app.listen(process.env.PORT || 3001)
+, () => console.log('Server started on port 3001');
 
