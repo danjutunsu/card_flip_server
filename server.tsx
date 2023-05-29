5a77a36a1be6bdfd89b359da12961e484dc411cf
danjutunsu <33231608+danjutunsu@users.noreply.github.com>
2023-05-28 21:35:23 -0700

    localhost

--- server.tsx
+++ server.tsx
@@ -200,7 +200,7 @@ server.listen(port, () => {
 
 const pool = new Pool({
     user: 'postgres',
-    host: '10.0.0.197',
+    host: 'localhost',
     database: 'trivia',
     password: dbpass,
     port: 5432, // the default PostgreSQL port
