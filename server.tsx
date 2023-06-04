a5dd41e2d8034e28654b8087f915920bee646436
danjutunsu <33231608+danjutunsu@users.noreply.github.com>
2023-06-03 23:04:02 -0700

    test

--- server.tsx
+++ server.tsx
@@ -48,7 +48,7 @@ const interval = setInterval(heartbeat, heartbeatInterval);
 
 setInterval(() => {
   clients.forEach((client) => {
-    client.send(JSON.stringify(new Date().toTimeString()));
+    client.send(new Date().toTimeString());
   });
 }, 1000);
 
